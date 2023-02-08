611dadeb242ae984a1a9d134fbdc613a549a47c1
WangJC97 <18516909028@163.com>
2023-02-08 13:28:46 +0800

    取消定时获取位置，改为按键获取位置
    取消定时获取位置，改为按键获取位置

--- WXCard.py
+++ WXCard.py
@@ -1,21 +1,19 @@
 import pyautogui
 import time
+import keyboard
 
-x = 1
-pyautogui.alert('确认【推送】按钮，5秒后获取鼠标位置')
-time.sleep(5)
+keyboard.wait('.')
 px,py = pyautogui.position()
-pyautogui.alert('【推送】位置获取完成')
-pyautogui.alert('确认【结束推送】按钮，5秒后获取鼠标位置')
-time.sleep(5)
+pyautogui.alert('推送位置获取完成')
+keyboard.wait('.')
 sx,sy = pyautogui.position()
-pyautogui.alert('【结束推送】位置获取完成')
+pyautogui.alert('结束推送位置获取完成')
 time.sleep(1)
-for x in range(0,1000):
+
+while 1:
     pyautogui.moveTo(px,py)
     pyautogui.click()
     time.sleep(30)
     pyautogui.moveTo(sx,sy)
     pyautogui.click()
     time.sleep(3)
-    x += 1
